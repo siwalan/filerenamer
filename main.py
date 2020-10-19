7e490f2c76388c303e1dbb9ebb7fd7dd20d2d739
Siwalan <59278902+siwalan@users.noreply.github.com>
2020-10-19 13:26:04 +0700

    Update main.py
    fixed bugs

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 import re
 counter =0
-
-directory_location = "Enter your Directory Path Here"
+dir_loc = "Enter your Directory Location Here"
 
 for root, subdirs, files in os.walk(dir_loc):
     for name in files:
-        file_name = name..split(".")
-        legalname = re.sub('[^A-Za-z0-9]+ ', ' ', file_name[0]) + file_name[1]
-        if legalname != file_name[0]:
+        file_name = name.split(".")
+        legal_name = re.sub('[^A-Za-z0-9]+ ', ' ', file_name[0]) +"."+ file_name[1]
+        if legal_name != name:
             counter = counter+1
-            os.rename(os.path.join(root,name), os.path.join(root,legalname))
-print(counter)
+            os.rename(os.path.join(root,name), os.path.join(root,legal_name))
+print(counter+' files renamed')
